5c20494d66c703c8af85f6af986abf25fcde6e9d
jbtestoni <jbn.testoni@gmail.com>
2024-06-14 17:28:10 -0300

    Add system implementation

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { processPayment } = require("./controllers/paymentController");
+const Order = require("./models/order");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/process-payment", (req, res) => {
+    const { id, productType, isUpgrade, isMembership } = req.body;
+    const order = new Order(id, productType, isUpgrade, isMembership);
+    processPayment(order);
+    res.json(order);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
