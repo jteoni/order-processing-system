80adff8650f35c8a3e9e92c5e2517e02cef0404d
jbtestoni <jbn.testoni@gmail.com>
2024-06-14 17:32:50 -0300

    Add more tests

--- tests/paymentRules.test.js
+++ tests/paymentRules.test.js
@@ -25,4 +25,41 @@ describe("Payment Rules", () => {
             "Generate commission payment to agent",
         ]);
     });
+
+    it("should activate new membership and notify owner", () => {
+        const order = new Order("3", "membership", false, true);
+        processPayment(order);
+        assert.deepStrictEqual(order.emailNotifications, [
+            "Activate membership and notify owner",
+        ]);
+    });
+
+    it("should apply upgrade to membership and notify owner", () => {
+        const order = new Order("4", "membership", true, true);
+        processPayment(order);
+        assert.deepStrictEqual(order.emailNotifications, [
+            "Apply upgrade and notify owner",
+        ]);
+    });
+
+    it('should include free "First Aid" video for "learning-ski" video product', () => {
+        const order = new Order("5", "video");
+        order.id = "learning-ski"; // Simulando que o ID é para o vídeo específico
+        processPayment(order);
+        assert.deepStrictEqual(order.shipmentSlip, [
+            'Include free "First Aid" video',
+        ]);
+    });
+
+    it("should generate shipment slip and commission for book and notify owner", () => {
+        const order = new Order("6", "book", true, false);
+        processPayment(order);
+        assert.deepStrictEqual(order.shipmentSlip, [
+            "Generate shipment slip for book",
+            "Duplicate shipment slip for royalties department",
+        ]);
+        assert.deepStrictEqual(order.commissionPayments, [
+            "Generate commission payment to agent",
+        ]);
+    });
 });
